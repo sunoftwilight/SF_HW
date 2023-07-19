22e1f4b8701dff4fbee012ba0475644c579a8bc0
sun <sixb_0417@hanmail.net>
2023-07-19 18:01:03 +0900

    hw

---
+++ 1_4.py
@@ -0,0 +1,8 @@
+# hw_1_4.py
+apple = '사과는 영어로 apple'
+banana = '바나나는 영어로 banana'
+kiwi = '키위는 영어로 kiwi'
+
+print(apple)
+print(banana)
+print(kiwi)
---
+++ 3_2.py
@@ -0,0 +1,8 @@
+def add_numbers(num1, num2):
+    return num1 + num2
+
+num1 = int(input())
+num2 = int(input())
+sum_result = add_numbers(num1, num2)
+
+print(f'{num1}과 {num2}를 인자로 넘긴 경우,\n{sum_result}')
