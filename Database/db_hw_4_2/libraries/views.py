79ccae606d8f896559f0924417eb212c921125a5
sun <v00417@naver.com>
2023-10-12 15:02:26 +0900

    1012

---
+++ Database/db_hw_4_2/libraries/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Book, Review
+
+
+def index(request):
+    books = Book.objects.all()
+    context = {
+        'books': books,
+    }
+    return render(request, 'libraries/index.html', context)
+
+
+def detail(request, pk):
+    book = Book.objects.get(pk=pk)
+    context = {
+        'book': book,
+    }
+    return render(request, 'libraries/detail.html', context)
