733297a865b191d7916615b25a622aa23696dd9f
sun <v00417@naver.com>
2023-11-10 17:53:15 +0900

    1110

---
+++ Vue/6_2/vue-project/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <MainPage />
+    <nav>
+      <RouterLink :to="{ name: 'some' }">Some</RouterLink>
+      <span> | </span>
+      <RouterLink :to="{ name: 'other' }">Other</RouterLink>
+    </nav>
+  </div>
+  <RouterView />
+</template>
+
+<script setup>
+import MainPage from '@/components/MainPage.vue'
+
+</script>
+
+<style scoped>
+
+</style>
